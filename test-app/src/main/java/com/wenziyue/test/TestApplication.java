9df28112597b93598d6869a0e70e7a9b41b732e7
wenziyue <wenziyue1984@163.com>
2025-04-22 14:35:54 +0800

    部署时排除test-app

--- test-app/src/main/java/com/wenziyue/test/TestApplication.java
+++ test-app/src/main/java/com/wenziyue/test/TestApplication.java
@@ -1,14 +1,17 @@
 package com.wenziyue.test;
 
+import com.wenziyue.mybatisplus.config.MyBatisPlusConfig;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 /**
  * @author wenziyue
  */
 @SpringBootApplication
 @MapperScan("com.wenziyue.test.mapper")
+@Import(MyBatisPlusConfig.class)
 public class TestApplication {
     public static void main(String[] args) {
         SpringApplication.run(TestApplication.class, args);
